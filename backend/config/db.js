03f1c0fa12e1fbedfcc2bad3aba562dce862952b
Jagdish Tilokani <jagdishtilokani835@gmail.com>
2021-10-02 17:49:52 +0530

    cloud database connected

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,25 @@
-import mongoose from 'mongoose'
-import User from '../models/usersModel.js'
+import mongoose from "mongoose";
+import User from "../models/usersModel.js";
 
 const connectDB = async () => {
-    try {
-        //database Name
-        const databaseName='MERN';
-        const con = await mongoose.connect(`mongodb://127.0.0.1:27017/${databaseName}`, { 
+  try {
+    //database Name
+    // const databaseName='MERN';
+    const connection_string =
+      "mongodb+srv://jagdish:jagdish@hospitalinfo.v4nlz.mongodb.net/HospitalInfo";
+    const con = await mongoose.connect(
+      // `mongodb://127.0.0.1:27017/${databaseName}`,
+      connection_string,
+      {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    });
-        console.log(`Database connected : ${con.connection.host}`)
-    } catch (error) {
-        console.error(`Error: ${error.message}`)
-        process.exit(1)
-    }
-}
+      }
+    );
+    console.log(`Database connected : ${con.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
-export default connectDB
+export default connectDB;
