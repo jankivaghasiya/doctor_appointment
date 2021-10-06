d02d7a64666cf31ceeabf12e863bb0fd37872869
Jagdish Tilokani <jagdishtilokani835@gmail.com>
2021-10-06 11:31:54 +0530

    folder structure corrected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,33 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
+import React, {Component, useEffect, useState} from 'react'
+// import axios from 'axios'
 
-const App = () => {
-  const [users, setUsers] = useState([])
-  const getData = async() => {
-    const res = await axios.get('/api/users')
-    setUsers(res.data)
-  }
+// const App = () => {
+//   const [users, setUsers] = useState([])
+//   const getData = async() => {
+//     const res = await axios.get('/api/users')
+//     setUsers(res.data)
+//   }
 
-  useEffect(() => {
-    getData()
-  }, [])
+//   useEffect(() => {
+//     getData()
+//   }, [])
  
-  return (
-    <div>
-      {users.map(u => <h4 key={u._id}>userName : {u.userName}</h4>)}
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       {users.map(u => <h4 key={u._id}>userName : {u.userName}</h4>)}
+//     </div>
+//   )
+// }
 
-export default App
+class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+};
+
+
+export default App;
