c849a9aafdec5094e2665aebdc727bff4c3654e4
PrincyGajera <princygajera121@gmail.com>
2021-10-18 00:54:43 +0530

    banner added

--- frontend/src/components/MainImage.jsx
+++ frontend/src/components/MainImage.jsx
@@ -3,12 +3,13 @@ import React, { Component } from "react";
 class Mainimage extends Component {
     render() {
         return (
+            <div>
             <img
                 src={require("../asset/slider_imgs/main.jpg").default}
                 alt="1"
-                width="100%"
-                height="20%"
+                className="mainimage"
             />
+            </div>
         );
     }
 }
